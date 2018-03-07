2c42996d0ad56e34fcf8863bb841f80c3c2b5012
Mikael Blomstrand <mbloms@kth.se>
2018-03-07 15:15:24 +0100

    method for acquireing locks
    aq

--- Unity/jam/Assets/Code/Intersection.cs
+++ Unity/jam/Assets/Code/Intersection.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 internal class Intersection
 {
-    //Is the quadrant free?
+    //Is the quadrant locked?
     private bool qne = false;
     private bool qnv = false;
     private bool qsv = false;
     private bool qse = false;
+
+    /*
+    Try to acquire locks for the specified quadrants.
+    Arguments: true to acquire lock, false else.
+    Returns: true if locks acquired, false else.
+     */
+    public bool Acquire(bool q1, bool q2, bool q3, bool q4)
+    {
+        if ( qne && q1
+          || qnv && q2
+          || qsv && q3
+          || qse && q4)
+        {
+            return false;
+        }
+        qne |= q1;
+        qnv |= q2;
+        qsv |= q3;
+        qse |= q4;
+        return true;
+    }
+
 
     public void Update()
     {
