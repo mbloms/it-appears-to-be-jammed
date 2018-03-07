6d94a32dc7c88d4db80f4f4a89c79089bbd62d9a
Mikael Blomstrand <mbloms@kth.se>
2018-03-07 14:52:37 +0100

    simple intersection
    update

---
+++ Unity/jam/Assets/Code/Intersection.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class Intersection
+{
+    //Is the quadrant free?
+    private bool qne = false;
+    private bool qnv = false;
+    private bool qsv = false;
+    private bool qse = false;
+
+    public void Update()
+    {
+        
+    }
+}
