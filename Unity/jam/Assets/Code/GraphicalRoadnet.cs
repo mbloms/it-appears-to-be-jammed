a445c514abac775bbe863782cd94974861ddd75e
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-15 18:21:40 +0100

    feat: add some `color` to the roads

--- Unity/jam/Assets/Code/GraphicalRoadnet.cs
+++ Unity/jam/Assets/Code/GraphicalRoadnet.cs
@@ -5,6 +5,9 @@ public class GraphicalRoadnet {
 
     public static float roadWidth = 0.5f;
     public static float roadThickness = 0.001f;
+    private static float lineWidth = 0.05f * roadWidth;
+
+    private static Color color_tarmac = new Color(0.15f, 0.15f, 0.15f);
 
     public GraphicalRoadnet(List<int[]> intersec, List<int[]> roads)
     {
@@ -22,10 +25,15 @@ public class GraphicalRoadnet {
 
     // create the intersection-object
     void CreateIntersection(float x, float z) {
+        GameObject edges = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        edges.GetComponent<Renderer>().material.color = Color.white;
+        edges.transform.position = new Vector3(x, 0, z);
+        edges.transform.localScale = new Vector3(roadWidth * 1.05f, roadThickness * 1.2f, roadWidth * 1.05f);
+
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.GetComponent<Renderer>().material.color = Color.red;
+        cube.GetComponent<Renderer>().material.color = color_tarmac;
         cube.transform.position = new Vector3(x, 0, z);
-        cube.transform.localScale = new Vector3(roadWidth, roadThickness*1.1f, roadWidth);
+        cube.transform.localScale = new Vector3(roadWidth - lineWidth, roadThickness*1.4f, roadWidth - lineWidth);
     }
 
     // create the road objects
@@ -46,11 +54,19 @@ public class GraphicalRoadnet {
         for (int i = 0; i*roadWidth < segments; i++) {
             float xpos = x1 + i * xdir * roadWidth + roadWidth * xdir;
             float zpos = z1 + i * zdir * roadWidth + roadWidth * zdir;
-            // draw the cube
+
+            // draw the road segment
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.GetComponent<Renderer>().material.color = Color.white;
+            cube.GetComponent<Renderer>().material.color = color_tarmac;
             cube.transform.position = new Vector3(xpos, 0, zpos);
-            cube.transform.localScale = new Vector3(roadWidth, roadThickness, roadWidth);
+            cube.transform.localScale = new Vector3(roadWidth * 1.05f, roadThickness, roadWidth * 1.05f);
+
+            // draw line in the middle of the segment
+            GameObject line = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            line.GetComponent<Renderer>().material.color = Color.white;
+            line.transform.position = new Vector3(xpos, 0, zpos);
+            line.transform.localScale = new Vector3(Mathf.Max(lineWidth * 6 * xdir, lineWidth), roadThickness * 1.3f, Mathf.Max(lineWidth * 6 * zdir, lineWidth));
+
         }
     }
 
