149b1233cc3f1f5ac521688e7ab97f2ccdfea556
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-15 17:15:30 +0100

    feat: scale velocity to real world values

--- Unity/jam/Assets/Code/Car.cs
+++ Unity/jam/Assets/Code/Car.cs
@@ -8,10 +8,13 @@ internal class Car
     private static float scale_factor = GraphicalRoadnet.roadWidth * 0.3f;
     private static float right_lane_offset = GraphicalRoadnet.roadWidth * 0.25f;
 
-    private static float speed_scaler = 0.0005f;
-    private static float max_speed = 50.0f;
-    private static float acceleration = 0.2f;//6.0f * speed_scaler;
-    private static float retardation = 1.0f;//40.0f * speed_scaler;
+    private static float car_length = GraphicalRoadnet.roadWidth * 0.8f;
+    private static float meter = car_length / 4.8f;
+
+    private static float speed_scaler = 0.0f;
+    private static float max_speed = 50.0f;     // km/h
+    private static float acceleration = 0.2f;   // km/s
+    private static float retardation = 2.0f;    // km/s
     private float speed = 0.0f;
 
     private float intersection_speed = max_speed * 0.5f;
@@ -193,6 +196,9 @@ internal class Car
 
     public void Drive()
     {
+        // update speed scaler for this iteration
+        speed_scaler = (meter / Main.FPS) / 3.6f;
+
         /** if waiting for OK to drive */
         if (waiting)
         {
